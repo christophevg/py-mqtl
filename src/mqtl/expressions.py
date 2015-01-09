ab87bca32200c403f551206aef2a88a145ae90e8
Christophe VG <contact@christophe.vg>
2015-01-10 00:34:42 +0100

    added unary, comparison and arithmetic operators

--- src/mqtl/expressions.py
+++ src/mqtl/expressions.py
@@ -8,6 +8,15 @@ class Exp(object):
   def eval(self, context=None):
     """Evaluates the expression in the given context."""
 
+class UnOpExp(Exp):
+  """Abstract base class for unary operators."""
+
+  def __init__(self, operand):
+    self.operand = operand
+
+  def eval(self, context=None):
+    return self._operate(self.operand.eval(context))
+
 class BinOpExp(Exp):
   """Abstract base class for binary operators."""
 
@@ -53,6 +62,12 @@ def val2bool(value):
   else:
     raise ValueError, "Value can not be represented as a boolean."
 
+# logical expressions
+
+class NotExp(UnOpExp):
+  def _operate(self, operand):
+    return not val2bool(operand)
+
 class AndExp(BinOpExp):
   def eval(self, context=None):
     # don't use _operate here, to keep short-cutting and-op
@@ -64,3 +79,72 @@ class OrExp(BinOpExp):
     # don't use _operate here, to keep short-cutting or-op
     return val2bool(self.left.eval(context)) \
         or val2bool(self.right.eval(context))
+
+# comparison expressions
+
+class ComparisonExp(BinOpExp):
+  """
+  Abstract base class for comparison expressions, preparing values for
+  comparison.
+  """
+  def _operate(self, left, right):
+    if is_bool(left) or is_bool(right):
+      return self._compare(val2bool(left), val2bool(right))
+    return self._compare(left, right)
+
+class EQExp(ComparisonExp):
+  def _compare(self, left, right):
+    return left == right
+
+class NEQExp(ComparisonExp):
+  def _compare(self, left, right):
+    return left != right
+
+class LTExp(ComparisonExp):
+  def _compare(self, left, right):
+    return left < right
+
+class LTEQExp(ComparisonExp):
+  def _compare(self, left, right):
+    return left <= right
+
+class GTExp(ComparisonExp):
+  def _compare(self, left, right):
+    return left > right
+
+class GTEQExp(ComparisonExp):
+  def _compare(self, left, right):
+    return left >= right
+
+# arithmatic expressions
+
+def val2number(value):
+  """Converts a value to a number."""
+  try:
+    return float(value)
+  except ValueError:
+    return float('NaN')
+
+class NegationExp(UnOpExp):
+  def _operate(self, operand):
+    return - val2number(operand)
+
+class AddExp(BinOpExp):
+  def _operate(self, left, right):
+    return val2number(left) + val2number(right)
+
+class SubExp(BinOpExp):
+  def _operate(self, left, right):
+    return val2number(left) - val2number(right)
+
+class MulExp(BinOpExp):
+  def _operate(self, left, right):
+    return val2number(left) * val2number(right)
+
+class DivExp(BinOpExp):
+  def _operate(self, left, right):
+    return val2number(left) / val2number(right)
+
+class ModExp(BinOpExp):
+  def _operate(self, left, right):
+    return val2number(left) % val2number(right)
--- src/mqtl/test_expressions.py
+++ src/mqtl/test_expressions.py
@@ -20,6 +20,17 @@ class TestExpressions(unittest.TestCase):
     right.eval.assert_called_with("context")
     op._operate.assert_called_with("left", "right")
 
+  def test_UnOpExp_dispatching_eval_to_operate_on_value(self):
+    operand = Exp()
+    operand.eval  = MagicMock(return_value="operand")
+
+    op = UnOpExp(operand)
+    op._operate = MagicMock()
+    op.eval("context")
+
+    operand.eval.assert_called_with("context")
+    op._operate.assert_called_with("operand")
+
   def test_is_bool(self):
     self.assertTrue(is_bool(True))
     self.assertTrue(is_bool(False))
@@ -70,6 +81,16 @@ class TestExpressions(unittest.TestCase):
     self.assertFalse(val2bool(False))
 
     self.assertRaises(ValueError, val2bool, {})
+
+  def test_NotExp_T_is_F(self):
+    operand = Exp()
+    operand.eval = MagicMock(return_value=True)
+    self.assertFalse(NotExp(operand).eval())
+
+  def test_NotExp_F_is_T(self):
+    operand = Exp()
+    operand.eval = MagicMock(return_value=False)
+    self.assertTrue(NotExp(operand).eval())
 
   def test_AndExp_TT_is_T(self):
     left, right = Exp(), Exp()
@@ -139,3 +160,140 @@ class TestExpressions(unittest.TestCase):
     # Or is short-cutting, but can't here
     left.eval.assert_called_with(None)
     right.eval.assert_called_with(None)
+
+  def test_ComparisonExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=1)
+    right.eval  = MagicMock(return_value=1.0)
+    op = ComparisonExp(left, right)
+    op._compare = MagicMock()
+    op.eval()
+    op._compare.assert_called_with(1, 1.0)
+
+  def test_ComparisonExp_with_boolean_conversion(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=1)
+    right.eval  = MagicMock(return_value=False)
+    op = ComparisonExp(left, right)
+    op._compare = MagicMock()
+    op.eval()
+    op._compare.assert_called_with(True, False)
+
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=True)
+    right.eval  = MagicMock(return_value="")
+    op = ComparisonExp(left, right)
+    op._compare = MagicMock()
+    op.eval()
+    op._compare.assert_called_with(True, False)
+
+  def test_EQExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=1)
+    right.eval  = MagicMock(return_value=1.0)
+    self.assertTrue(EQExp(left, right).eval())
+
+    left.eval   = MagicMock(return_value="a")
+    right.eval  = MagicMock(return_value=123)
+    self.assertFalse(EQExp(left, right).eval())
+
+  def test_NEQExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=1)
+    right.eval  = MagicMock(return_value=1.0)
+    self.assertFalse(NEQExp(left, right).eval())
+
+    left.eval   = MagicMock(return_value="a")
+    right.eval  = MagicMock(return_value=123)
+    self.assertTrue(NEQExp(left, right).eval())
+
+  def test_LTExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=1)
+    right.eval  = MagicMock(return_value=3)
+    self.assertTrue(LTExp(left, right).eval())
+
+    left.eval   = MagicMock(return_value=3)
+    right.eval  = MagicMock(return_value=3)
+    self.assertFalse(LTExp(left, right).eval())
+
+  def test_LTEQExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=1)
+    right.eval  = MagicMock(return_value=3)
+    self.assertTrue(LTExp(left, right).eval())
+
+    left.eval   = MagicMock(return_value=3)
+    right.eval  = MagicMock(return_value=3)
+    self.assertTrue(LTEQExp(left, right).eval())
+
+  def test_GTExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=3)
+    right.eval  = MagicMock(return_value=1)
+    self.assertTrue(GTExp(left, right).eval())
+
+    left.eval   = MagicMock(return_value=3)
+    right.eval  = MagicMock(return_value=3)
+    self.assertFalse(GTExp(left, right).eval())
+
+  def test_GTEQExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=3)
+    right.eval  = MagicMock(return_value=1)
+    self.assertTrue(GTEQExp(left, right).eval())
+
+    left.eval   = MagicMock(return_value=3)
+    right.eval  = MagicMock(return_value=3)
+    self.assertTrue(GTEQExp(left, right).eval())
+
+  def test_val2number(self):
+    self.assertEqual(val2number(123), 123)
+    self.assertEqual(val2number(123.0), 123)
+    self.assertEqual(val2number("123"), 123)
+    self.assertEqual(val2number("123.0"), 123)
+    self.assertTrue(math.isnan(val2number("abc")))
+
+  def test_NegationExp(self):
+    operand = Exp()
+    operand.eval = MagicMock(return_value=123)
+    self.assertEqual(NegationExp(operand).eval(), -123)
+
+    operand.eval = MagicMock(return_value=-123)
+    self.assertEqual(NegationExp(operand).eval(), 123)
+
+  def test_AddExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=15)
+    right.eval  = MagicMock(return_value=8)
+    self.assertEqual(AddExp(left, right).eval(), 23)
+
+  def test_SubExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=15)
+    right.eval  = MagicMock(return_value=8)
+    self.assertEqual(SubExp(left, right).eval(), 7)
+
+  def test_MulExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=15)
+    right.eval  = MagicMock(return_value=8)
+    self.assertEqual(MulExp(left, right).eval(), 120)
+
+  def test_DivExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=18)
+    right.eval  = MagicMock(return_value=3)
+    self.assertEqual(DivExp(left, right).eval(), 6)
+
+  def test_SubExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=15)
+    right.eval  = MagicMock(return_value=8)
+    self.assertEqual(SubExp(left, right).eval(), 7)
+
+  def test_ModExp(self):
+    left, right = Exp(), Exp()
+    left.eval   = MagicMock(return_value=15)
+    right.eval  = MagicMock(return_value=4)
+    self.assertEqual(ModExp(left, right).eval(), 3)
